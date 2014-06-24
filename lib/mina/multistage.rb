c293a44656e0ffc67b27368abfa17cdb3211e05c
Alexey Chernenkov <laise@pisem.net>
2014-06-24 22:10:11 +0600

    Get rid of development stage

--- lib/mina/multistage.rb
+++ lib/mina/multistage.rb
@@ -1,35 +1,25 @@
 require 'fileutils'
 
-location = fetch(:stage_dir, "config/deploy")
+location = fetch(:stage_dir, 'config/deploy')
 
 unless exists?(:stages)
-  set :stages, Dir["#{location}/*.rb"].map { |f| File.basename(f, ".rb") }
+  set :stages, Dir["#{location}/*.rb"].map { |f| File.basename(f, '.rb') }
 end
 
 stages.each do |name|
   desc "Set the target stage to `#{name}'."
   task(name) do
     set :stage, name.to_sym
-
     file = "#{location}/#{stage}.rb"
     load file
   end
 end
 
-if stages && stages.count > 0
-  if stages.include? 'development'
-    invoke :development
-  else
-    invoke stages.first
-  end
-end
-
 namespace :multistage do
-  desc "Create development, staging, and production stage files"
+  desc 'Create staging and production stage files'
   task :init do
-    Dir.mkdir location if !File.exists? location
-
-    %w{ development staging production }.each do |stage|
+    Dir.mkdir_p location if !File.exists? location
+    %w{staging production}.each do |stage|
       stagefile = File.join(location, "#{stage}.rb")
       if !File.exists?(stagefile)
         File.open(stagefile, 'w') do |f|
